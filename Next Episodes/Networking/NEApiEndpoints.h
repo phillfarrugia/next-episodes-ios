a305d5fc2033d3b7bfda81a6559dcdd21eb410f6
Phill Farrugia <me@phillfarrugia.com>
2015-01-24 15:26:45 +1100

    Prefix Headers

--- Next Episodes/Networking/NEApiEndpoints.h
+++ Next Episodes/Networking/NEApiEndpoints.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 Phill Farrugia. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
 @interface NEApiEndpoints : NSObject
 
 - (NSURLRequest *)trendingShowsRequest;
--- Next Episodes/Networking/NEJsonDataModelDecoder.h
+++ Next Episodes/Networking/NEJsonDataModelDecoder.h
@@ -6,8 +6,6 @@
 //  Copyright (c) 2015 Phill Farrugia. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-
 @class MTLModel;
 
 @protocol NEJsonDataModelDecoder
