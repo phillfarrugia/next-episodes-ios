108743b3912de133614e78bfa8584221e111ff67
Phill Farrugia <me@phillfarrugia.com>
2015-01-24 14:38:15 +1100

    Custom UIColor Category for Global Colours

---
+++ Next Episodes/Categories/UIColor+NextEpisodes.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+NextEpisodes.h
+//  Next Episodes
+//
+//  Created by Phill Farrugia on 24/01/2015.
+//  Copyright (c) 2015 Phill Farrugia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (NextEpisodes)
+
++ (UIColor *)navigationColor;
+
+@end
---
+++ Next Episodes/Categories/UIColor+NextEpisodes.m
@@ -0,0 +1,18 @@
+//
+//  UIColor+NextEpisodes.m
+//  Next Episodes
+//
+//  Created by Phill Farrugia on 24/01/2015.
+//  Copyright (c) 2015 Phill Farrugia. All rights reserved.
+//
+
+#import "UIColor+NextEpisodes.h"
+
+@implementation UIColor (NextEpisodes)
+
++ (UIColor *)navigationColor
+{
+    return [UIColor colorWithRed:204 / 255.0f green:110 / 255.0f blue:131 / 255.0f alpha:1.0f];
+}
+
+@end
